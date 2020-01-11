c98e163a948f23344611e93f022a7f0383bc31ef
ilhamabdulm <a.marzmellow21@gmail.com>
2020-01-12 01:57:47 +0700

    add some client side

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const UserRoutes = require('./user')
-
-router.use('/user', UserRoutes)
-
-module.exports = router
--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const UserController = require('../controllers/user')
-
-// regist & login
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
-
-module.exports = router
