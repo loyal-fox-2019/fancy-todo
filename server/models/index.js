5cb6327b3c39d7d2d6ec67f5c6269013db27c884
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-13 07:06:21 +0700

    adding index component in models server side

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,7 @@
 'use strict'
+
+const UserController = require('./user')
+const TodoController = require('./todo')
+const GroupController = require('./group')
+
+module.exports = { UserController, TodoController, GroupController }
