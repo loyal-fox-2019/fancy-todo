88b85eae4ff8b8ecb8c0be100af1df87d27c6b64
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2020-01-13 08:57:49 +0700

    jap hendy wijaya - deploy client

--- server/config/mongoAtlas.js
+++ server/config/mongoAtlas.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://jap:jap@cluster0-ki8rr.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true,  })
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('mongoose connected')
 });
+
+
--- server/helper/jwt.js
+++ server/helper/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const secret = process.env.JWT_SECRET
+const secret = process.env.JWT_SECRET || "kacanggoreng"
 
 function generateToken( payload )
   {
