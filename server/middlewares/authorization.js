1cd5d069f598e8ea3e22f2ce8105fcc7a6dc8a33
mustaqinIshak <mustaqinishak@gmail.com>
2020-01-13 08:10:48 +0700

    authorization.js: for check user is still available

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,2 +1,17 @@
 'use strict'
 
+const User = require('../models/users')
+
+module.exports = function (res, req, next) {
+
+    User.findById({id: req.decode.id})
+    .then(result => {
+        if(!result){
+            throw {status: 404, message: 'not found'}
+        }
+        else {
+            next()
+        }
+    })
+    .catch(next)
+}
