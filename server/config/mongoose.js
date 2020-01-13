18207f026f0df87f8eedaba2717cd9fe909fff5d
michaelwp <michael.wenceslaus@gmail.com>
2020-01-13 18:38:46 +0700

    mongoose file

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect('mongodb+srv://michaelwp:michaelwp@cluster0-sjji7.gcp.mongodb.net/fancytodo?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false,
