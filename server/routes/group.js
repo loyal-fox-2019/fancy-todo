5768a30d7161625e59f0c4c5a0a9df5dfbcde2f0
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-13 07:23:11 +0700

    adding group router in server side

--- server/routes/group.js
+++ server/routes/group.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const express = require('express')
+const Router = express.Router()
+const { GroupController, TodoController } = require('../controllers')
+const { authorizeGroup, authorizeLeaderGroup } = require('../middlewares/auth')
+
+Router.post('/', GroupController.createGroup)
+
+Router.use('/:id', authorizeGroup)
+Router.get('/:id', GroupController.getOneGroup)
+Router.post('/:id/todos', TodoController.createTodo)
+Router.get('/:id/todos', TodoController.getAllGroupTodos)
+Router.delete('/:id/leave', GroupController.LeaveGroup)
+
+Router.use('/:id', authorizeLeaderGroup)
+Router.patch('/:id', GroupController.editGroupName)
+Router.delete('/:id', GroupController.deleteGroup)
+Router.patch('/:id/members', GroupController.inviteMember)
+Router.delete('/:id/members/:member_id', GroupController.kickMember)
+
+module.exports = groups
