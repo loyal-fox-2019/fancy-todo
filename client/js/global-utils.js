0ec78f4476089c654374d35628d2782827f988f7
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-13 07:51:04 +0700

    adding global utility in cliet side

--- client/js/global-utils.js
+++ client/js/global-utils.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const baseUrl = 'http://localhost:3000'
+
+function toast(title, timer) {
+  return Swal.fire({
+    title: title == 'Loading' ? '' : title,
+    html:
+      title == 'Loading'
+        ? title + '... <span class="spinner-border spinner-border-sm"></span>'
+        : undefined,
+    timer,
+    showConfirmButton: false,
+    toast: true,
+    position: 'bottom',
+    showClass: {
+      popup: 'animated fadeInUp faster'
+    },
+    hideClass: {
+      popup: 'animated fadeOutDown faster'
+    }
+  })
+}
