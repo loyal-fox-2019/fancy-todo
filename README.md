b6bae2ea76ab0126b79a24e31dd3247da0d4272d
japhendywijaya <jap_hendy_wijaya@yahoo.co.id>
2020-01-13 19:19:34 +0700

    jap hendy wijaya - update documentation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fancy-todo
+
+link dokumentasi
+https://documenter.getpostman.com/view/9584254/SWLia6cy?version=latest#intro
--- client/config/ajax.js
+++ client/config/ajax.js
@@ -1,1 +1,2 @@
-const baseUrl = "http://localhost:3000"
+// const baseUrl = "http://localhost:3000"
+const baseUrl = "http://35.226.138.221:3000"
